49156d7fad64636e87f962caffaed98ac637d912
Sofie Aasheim <sofieaaf@gmail.com>
2020-10-16 15:43:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 app.layout = html.Div([
-    html.H2('Hello World dette er en test'),
+    html.H2('Hello World dette er en test håper dette funker'),
     dcc.Dropdown(
         id='dropdown',
         options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
