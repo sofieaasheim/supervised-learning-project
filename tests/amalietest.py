26fc8b6ae078b5a5b86602daa862b7cdfcd2c8b9
Amalie Berge Holm <eaholm@stud.ntnu.no>
2020-10-23 13:21:23 +0200

    Amalie sin test fil

--- tests/amalietest.py
+++ tests/amalietest.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # Import the entire data sets
-math_df = pd.read_csv("./supervised-learning-project/data/student-mat.csv", sep=";")
+math_df = pd.read_csv("../supervised-learning-project/data/student-mat.csv", sep=";")
 
 # Divide into parameters and responses
 parameter_df = math_df.drop(["G1", "G2", "G3"], axis=1)
