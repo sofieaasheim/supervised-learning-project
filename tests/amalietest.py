73c9c9c22707424f84650ba75776138270a3cc65
Amalie Berge Holm <eaholm@stud.ntnu.no>
2020-10-23 15:36:33 +0200

    yes/no to 1/0

--- tests/amalietest.py
+++ tests/amalietest.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
 # Import the entire data sets
-math_df = pd.read_csv("../supervised-learning-project/data/student-mat.csv", sep=";")
+math_df = pd.read_csv("./supervised-learning-project/data/student-mat.csv", sep=";")
 
 # Divide into parameters and responses
 parameter_df = math_df.drop(["G1", "G2", "G3"], axis=1)
 response_df = math_df[["G1", "G2", "G3"]]
+
+math_df.replace(('yes', 'no'), (1, 0), inplace=True)
+
+print(math_df)
--- tests/sofietest.py
+++ tests/sofietest.py
@@ -6,3 +6,5 @@ math_df = pd.read_csv("./supervised-learning-project/data/student-mat.csv", sep=
 # Divide into parameters and responses
 parameter_df = math_df.drop(["G1", "G2", "G3"], axis=1)
 response_df = math_df[["G1", "G2", "G3"]]
+
+print(math_df)
