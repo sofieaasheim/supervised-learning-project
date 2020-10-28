980e1cd0f7128beef4b69f0ed6f0867a6e2308b5
Amalie Berge Holm <eaholm@stud.ntnu.no>
2020-10-28 08:11:40 +0100

    Testing dummy variables

--- tests/amalietest.py
+++ tests/amalietest.py
@@ -7,6 +7,12 @@ math_df = pd.read_csv("./supervised-learning-project/data/student-mat.csv", sep=
 parameter_df = math_df.drop(["G1", "G2", "G3"], axis=1)
 response_df = math_df[["G1", "G2", "G3"]]
 
+# Boolean variables
 math_df.replace(('yes', 'no'), (1, 0), inplace=True)
 
-print(math_df)
+# Dummie variables, test with sex column 
+sex_df = math_df[["sex"]]
+dummies = pd.get_dummies(sex_df)
+
+# print(math_df)
+print(dummies)
