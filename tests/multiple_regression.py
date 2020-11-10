9fc45a4dfac089b807b783fb37f988d7d16b23e3
Sofie Aasheim <sofieaaf@gmail.com>
2020-11-10 16:32:20 +0100

    Add another comment

--- tests/multiple_regression.py
+++ tests/multiple_regression.py
@@ -48,4 +48,5 @@ print(multiple_regression(df_regr, parameter_list_2))
 # Now all parameters have a p-value of less than 0.05, which indicates that all parameters
 # are likely to be a meaningful addition to the model.
 
-# Step 3: Training and testing the model ??? Er dette riktig rekkefølge?
+# Step 3: Training and testing the model ??? Er dette riktig rekkefølge? Kan bytte til
+# å bruke sklearn nå hvis det er lettere evt, tror det er mer tilpasset training/test
